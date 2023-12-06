e1ac2f78a8e45b57d6c7144f947d7df9eda679e3
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-12-05 18:40:41 -0800

    Remove license-file

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use model::Model;
 use pose_detector::PoseDetector;
 use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::JsFuture;
-use web_sys::console::{log_1, log_2};
 
 mod bindings;
 mod call_method;
@@ -28,13 +27,10 @@ impl ToString for BackendName {
 }
 
 pub async fn create_detector(model: Model) -> Result<PoseDetector, JsValue> {
-    let name = model.get_name();
-    let detector_js_value = JsFuture::from(bindings::create_detector(&model.get_name()[..], &{
-        let c = model.get_config();
-        log_1(&"create_detector".into());
-        log_2(&name[..].into(), &c);
-        c
-    }))
+    let detector_js_value = JsFuture::from(bindings::create_detector(
+        &model.get_name()[..],
+        &model.get_config(),
+    ))
     .await?;
     Ok(PoseDetector::from(detector_js_value))
 }
