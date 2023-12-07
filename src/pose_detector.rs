39fcc2c81c61adbc83738b5f495727b20d8a50bc
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-12-07 08:49:28 -0800

    Add estimation config

--- src/pose_detector.rs
+++ src/pose_detector.rs
@@ -1,4 +1,5 @@
 use crate::{call_method::call_method, pose::Pose};
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::{
     js_sys::{Array, Object, Promise},
@@ -43,13 +44,62 @@ impl Into<JsValue> for PoseDetector {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct CommonEstimationConfig {
+    pub max_poses: Option<u32>,
+    pub flip_horizontal: Option<bool>,
+}
+impl Into<JsValue> for CommonEstimationConfig {
+    fn into(self) -> JsValue {
+        serde_wasm_bindgen::to_value(&self).unwrap()
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PoseNetEstimationConfig {
+    #[serde(skip_serializing)]
+    pub common_config: CommonEstimationConfig,
+    pub score_threshold: Option<f64>,
+    pub nms_radius: Option<f64>,
+}
+impl Into<JsValue> for PoseNetEstimationConfig {
+    fn into(self) -> JsValue {
+        let common_config: Object = serde_wasm_bindgen::to_value(&self.common_config)
+            .unwrap()
+            .dyn_into()
+            .unwrap();
+        let pose_net_config: Object = serde_wasm_bindgen::to_value(&self)
+            .unwrap()
+            .dyn_into()
+            .unwrap();
+        Object::assign2(&Object::new(), &common_config, &pose_net_config).into()
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum EstimationConfig {
+    PoseNet(PoseNetEstimationConfig),
+    BlazePoseOrMoveNet(CommonEstimationConfig),
+}
+impl Into<JsValue> for EstimationConfig {
+    fn into(self) -> JsValue {
+        match self {
+            EstimationConfig::PoseNet(config) => config.into(),
+            EstimationConfig::BlazePoseOrMoveNet(config) => config.into(),
+        }
+    }
+}
+
 impl PoseDetector {
     pub async fn estimate_poses(
         &self,
         image: &JsValue,
+        config: EstimationConfig,
         timestamp: Option<i32>,
     ) -> Result<Vec<Pose>, JsValue> {
-        let inputs = Array::from_iter(vec![image, &JsValue::UNDEFINED, &timestamp.into()].iter());
+        let inputs = Array::from_iter(vec![image, &config.into(), &timestamp.into()].iter());
         let poses = JsFuture::from(Promise::from(call_method(
             &self.js_value,
             &"estimatePoses".into(),
