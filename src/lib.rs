258acc5393118219dc5695288f1f6864a4191cbd
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-12-12 11:48:49 -0800

    Remove unused enum

--- src/lib.rs
+++ src/lib.rs
@@ -10,23 +10,6 @@ pub mod pose;
 pub mod pose_detector;
 pub mod util;
 
-pub enum BackendName {
-    Webgl,
-    Cpu,
-    Tensorflow,
-}
-
-impl ToString for BackendName {
-    fn to_string(&self) -> String {
-        match self {
-            BackendName::Cpu => "cpu",
-            BackendName::Webgl => "webgl",
-            BackendName::Tensorflow => "tensorflow",
-        }
-        .into()
-    }
-}
-
 pub async fn create_detector(model: ModelWithConfig) -> Result<PoseDetector, JsValue> {
     let name = &model.get_name()[..];
     let config = model.get_config();
